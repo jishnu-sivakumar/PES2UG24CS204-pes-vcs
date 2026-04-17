c2ce2f8871c4950817a1157282b7185c0f9abbec
Jishnu <jishnusiva06@gmail.com>
2026-04-17 19:39:57 +0530

    using info from .pes/HEAD and tree info we make a huge string that represents the commit status(not working code)

--- commit.c
+++ commit.c
@@ -194,8 +194,18 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+      Commit c;
+    memset(&c, 0, sizeof(Commit));
+
+    if (tree_from_index(&c.tree) != 0) {
+        fprintf(stderr, "[DEBUG] tree_from_index failed.\n");
+        return -1;
+    }
+
+    if (head_read(&c.parent) == 0) {
+        c.has_parent = 1;
+    } else {
+        c.has_parent = 0; 
+    }
+
 }
